26369bd6d2a346734562ed968e70370ed209a3db
zhavens <havensz@myumanitoba.ca>
2021-11-10 12:23:08 -0600

    Adding profiling option.

--- src/convex_hull_main.py
+++ src/convex_hull_main.py
@@ -19,6 +19,8 @@ $ convex_hull_main.py \
     --verbose=1
 """
 
+import cProfile
+import pstats
 import os
 from datetime import datetime
 from typing import List, Text
@@ -34,7 +36,7 @@ import convex_hull
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_enum("algo", "gw", ["gw", "dc", "grahams", "chans", "test"],
+flags.DEFINE_enum("algo", "gw", ["gw", "dc", "grahams", "chans"],
                   "The algorithm to generate the convex hull with.")
 
 flags.DEFINE_string(
@@ -44,8 +46,13 @@ flags.DEFINE_string(
     "hull_outfile", None, "The output file path for writing.")
 
 flags.DEFINE_string(
-    "stats_outfile", None, "The path to a file to append runtime stats to."
-)
+    "stats_outfile", None, "The path to a file to append runtime stats to.")
+
+flags.DEFINE_bool("profile_algo", False,
+                  "Whether to profile the hull construction algorithm.")
+flags.DEFINE_string("profile_outfile", None,
+                    "The path to dump profiling information if enabled. If "
+                    "unspecified, output will be dumped to stdout.")
 
 flags.DEFINE_bool("show_plot", False,
                   "Whether to show the plot and hull after construction.")
@@ -63,22 +70,33 @@ def main(argv):
     points = util.fetch_input_points(FLAGS.infile)
     logging.vlog(1, f"Input Points: {points}")
 
-    start_time = datetime.now()
-    hull = None
+    algo = None
     if FLAGS.algo == "gw":
-        hull = convex_hull.gift_wrapping(points)
+        algo = convex_hull.gift_wrapping
     elif FLAGS.algo == "dc":
-        hull = convex_hull.divide_and_conquer(points)
+        algo = convex_hull.divide_and_conquer
     elif FLAGS.algo == "grahams":
-        hull = convex_hull.grahams_scan(points)
+        algo = convex_hull.grahams_scan
     elif FLAGS.algo == "chans":
-        hull = convex_hull.chans_algorithm(points)
-    elif FLAGS.algo == "test":
-        hull = points
+        algo = convex_hull.chans_algorithm
     else:
         raise NotImplementedError(
             f"--algo=\"{FLAGS.algo}\" not supported.")
-    runtime = (datetime.now() - start_time).total_seconds()
+    hull = None
+    if FLAGS.profile_algo:
+        with cProfile.Profile() as profiler:
+            hull = algo(points)
+        stats = pstats.Stats(profiler)
+        stats.strip_dirs()
+        stats.sort_stats(pstats.SortKey.CUMULATIVE)
+        stats.print_stats()
+        if FLAGS.profile_outfile:
+            stats.dump_stats(FLAGS.profile_outfile)
+    else:
+        start_time = datetime.now()
+        hull = algo(points)
+        runtime = (datetime.now() - start_time).total_seconds()
+        logging.info(f"Constructed the hull in {runtime}s.")
 
     if FLAGS.show_plot:
         util.show_plot(points, hulls=[hull])
@@ -87,8 +105,6 @@ def main(argv):
         util.write_points(hull, FLAGS.hull_outfile)
     else:
         logging.info(f"Hull Points: {hull}")
-
-    logging.info(f"Constructed the hull in {runtime}s.")
 
     if not convex_hull.validate_hull(hull, points):
         logging.error("Error: constructed hull is not convex.")
