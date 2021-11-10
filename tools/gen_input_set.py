6dea5544433bfbec7f679e6415e50fe34e23c7ef
zhavens <havensz@myumanitoba.ca>
2021-11-10 11:22:18 -0600

    Adding clustered point generation.

--- tools/gen_input_set.py
+++ tools/gen_input_set.py
@@ -1,25 +1,7 @@
-"""Main script for convex hull construction.
-
-Sample Invocations ----------------------
-
-Generates a hull using Chan's distributionrithm, saves the hull to a file, and appends
-runtime statistics to a CSV file:
-$ convex_hull_main.py \
-    --distribution=chan \
-    --infile=inputs/testpoints
-    --hull_outfile=outputs/hull
-    --stats_outfile=results/results.csv
-
-Generates a hull using Graham's Scan and displays a plot of the results and
-logs verbosely.
-$ convex_hull_main.py \
-    --distribution=graham \
-    --infile=inputs/testpoints
-    --show_plot
-    --verbose=1
-"""
+"""Main script for generating input sets."""
 
 from datetime import datetime
+import math
 import os
 import random
 from typing import List, Text
@@ -33,7 +15,7 @@ import util
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_enum("distribution", "uniform", ["uniform", "normal"],
+flags.DEFINE_enum("distribution", "uniform", ["uniform", "normal", "clustered"],
                   "The distribution type for the generated points.")
 
 flags.DEFINE_integer(
@@ -49,6 +31,11 @@ flags.DEFINE_float("normal_stddev", None,
                    "The standarad deviation to use for a normal distribution. "
                    "Defaults to 1/10 of --max_coord.")
 
+flags.DEFINE_integer("clustered_num_clusters", 1, "The number of clusters to "
+                     "generate points around.")
+flags.DEFINE_float("clustered_max_dist_from_center", 10, "How far points can "
+                   "be from the center of a cluster.")
+
 flags.DEFINE_bool("show_plot", False,
                   "Whether to show the points after generation.")
 
@@ -59,11 +46,11 @@ def print_usage():
 Usage: gen_input_set.py --distribution=[dist] --num_points=[num_points] --outfile=[infile]""")
 
 
-def uniform_points(num_points: int, max_value: float):
+def uniform_points(num_points: int, max_coord: float):
     points = []
     for i in range(num_points):
-        points.append(Point(random.uniform(0, max_value),
-                            random.uniform(0, max_value)))
+        points.append(Point(random.uniform(0, max_coord),
+                            random.uniform(0, max_coord)))
     return points
 
 
@@ -72,6 +59,22 @@ def normal_points(num_points: int, mean: float, stddev: float):
     for i in range(num_points):
         points.append(Point(random.gauss(mean, stddev),
                             random.gauss(mean, stddev)))
+    return points
+
+
+def clustered_points(num_points: int, num_clusters: int, max_coord: float,
+                     max_from_center: float):
+    points = []
+    centers = [Point(random.uniform(0, max_coord),
+                     random.uniform(0, max_coord)) for i in range(num_clusters)]
+
+    for i in range(num_points):
+        center = int(random.uniform(0, num_clusters))
+        dist = random.uniform(0, max_from_center)
+        angle = random.uniform(0, math.pi*2)
+        points.append(Point(centers[center].x + (math.cos(angle)*dist),
+                            centers[center].y + (math.sin(angle)*dist)))
+
     return points
 
 
@@ -84,6 +87,11 @@ def main(argv):
     elif FLAGS.distribution == "normal":
         stddev = FLAGS.normal_stddev if FLAGS.normal_stddev else FLAGS.max_coord/10
         points = normal_points(FLAGS.num_points, FLAGS.max_coord/2, stddev)
+    elif FLAGS.distribution == "clustered":
+        points = clustered_points(FLAGS.num_points,
+                                  FLAGS.clustered_num_clusters,
+                                  FLAGS.max_coord,
+                                  FLAGS.clustered_max_dist_from_center)
     else:
         raise NotImplementedError(
             f"--distribution=\"{FLAGS.distribution}\" not supported.")
@@ -91,7 +99,9 @@ def main(argv):
     if FLAGS.show_plot:
         util.show_plot(points, None)
 
-    util.write_points(points, FLAGS.outfile)
+    if FLAGS.outfile:
+        util.write_points(points, FLAGS.outfile)
+        logging.info(f"Wrote points to {FLAGS.outfile}")
 
 
 if __name__ == "__main__":
@@ -100,6 +110,6 @@ if __name__ == "__main__":
     flags.register_validator('num_points',
                              lambda v: v > 0,
                              message='--num_points must be positive.')
-    flags.mark_flag_as_required('outfile')
+    # flags.mark_flag_as_required('outfile')
 
     app.run(main)
